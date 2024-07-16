5ee2dd70442331e7cc6e8f6773669c6ca7fdf6b2
Apaar Saini <apaarsaini007@gmail.com>
2024-07-16 10:20:00 +0530

    DOM commit
    The Separation of Concerns: Structure vs Style vs Behaviour
    Text Manipulation and the Text Content Property
    Manipulating HTML Element Attributes

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -1,2 +1,6 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML="Bruhhh"
 document.querySelector("button").style.backgroundColor="yellow";
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").innerHTML="<em>Hello</em>";
+document.querySelector("h1").textContent="Helloo!!";
+document.querySelector("a").setAttribute("href","https://www.bing.com");
