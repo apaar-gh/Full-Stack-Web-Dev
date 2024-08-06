df57a142bdb903f030913bb25752338634b845c0
Apaar Saini <apaarsaini007@gmail.com>
2024-08-06 17:24:08 +0530

    express

--- 8. Express.js/8.4 Middleware/index4.js
+++ 8. Express.js/8.4 Middleware/index4.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
