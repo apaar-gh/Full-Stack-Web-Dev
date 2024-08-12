1645ee7f80d5e6167d47706ff3509af016759548
Apaar Saini <apaarsaini007@gmail.com>
2024-08-12 20:37:00 +0530

    ejs commit

--- 9. EJS/9.2+Passing+Data/index.js
+++ 9. EJS/9.2+Passing+Data/index.js
@@ -6,9 +6,20 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs",{
+    nameLen: 0,
+  })
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+    var fName= req.body["fName"];
+    var lName= req.body["lName"];
+    var len = fName.length+lName.length;
+  res.render("index.ejs",{
+    nameLen: len,
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
